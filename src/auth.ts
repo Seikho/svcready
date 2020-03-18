ac4a1803664b64b35d70d2f186569178c3511337
Carl Winkler <carl@winkler.id.au>
2020-03-18 13:54:52 +0800

    Replace bcrypt with bcryptjs

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import * as jwt from 'jsonwebtoken'
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs'
 import { handle } from './handler'
 import { StatusError, AuthConfig, ServiceRequest } from './types'
 
