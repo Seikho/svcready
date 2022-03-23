534f583ec89544b06459c18e2d4aacf4e1f4adcc
Carl Winkler <carl@winkler.id.au>
2022-03-23 12:48:17 +0800

    Support called res with return value from handle - Only if next not called and headers not sent - Add wrap as handle alias

--- src/handler.ts
+++ src/handler.ts
@@ -3,11 +3,22 @@ import { Handler } from './types'
 
 export function handle(handler: Handler) {
   const wrapped: express.RequestHandler = async (req, res, next) => {
+    let nextCalled = false
+    const wrappedNext = (err?: any) => {
+      nextCalled = true
+      next(err)
+    }
+
     try {
-      await handler(req as any, res, next)
+      const result = await handler(req as any, res, wrappedNext)
+      if (!res.headersSent && !nextCalled) {
+        res.json(result)
+      }
     } catch (ex) {
       next(ex)
     }
   }
   return wrapped
 }
+
+export const wrap = handle
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './types'
 export * from './api'
-export { handle } from './handler'
+export { handle, wrap } from './handler'
 export { logger, createLogger } from './log'
 export { encrypt, createToken } from './auth'
 export { SvcSocket } from './socket'
